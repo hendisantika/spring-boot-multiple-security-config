52923a123ad4ecffc91de9b1ea37c1f45256a0df
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-03 06:35:00 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/SpringBootMultipleSecurityConfigApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleSecurityConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMultipleSecurityConfigApplication.class, args);
+    }
+
+}
