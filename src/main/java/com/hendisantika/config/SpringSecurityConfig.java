6ba1a3dee33b1514cf21b3aa38931ae33d68e1fb
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-03 06:46:32 +0700

    #13 - Add AdminSpringSecurityConfiguration

--- src/main/java/com/hendisantika/config/SpringSecurityConfig.java
+++ src/main/java/com/hendisantika/config/SpringSecurityConfig.java
@@ -3,7 +3,9 @@ package com.hendisantika.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -40,6 +42,27 @@ public class SpringSecurityConfig {
                     .failureUrl("/user-login?error")
                     .permitAll();
             // @formatter:on
+        }
+    }
+
+    @Configuration
+    public class AdminSpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    .authorizeRequests()
+                    .antMatchers("/user-login/**").permitAll()
+                    .anyRequest().hasRole("ADMIN")
+                    .and()
+                    .formLogin(Customizer.withDefaults());
+            // @formatter:on
+        }
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers("/webjars/**");
         }
     }
 
