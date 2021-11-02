37b2f619e73947a8e6fc290003ae4fa8f00ec1ca
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-03 06:46:06 +0700

    #12 - Add UserSpringSecurityConfiguration

--- src/main/java/com/hendisantika/config/SpringSecurityConfig.java
+++ src/main/java/com/hendisantika/config/SpringSecurityConfig.java
@@ -1,6 +1,11 @@
 package com.hendisantika.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,7 +22,27 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
  * Time: 6:44 AM
  * To change this template use File | Settings | File Templates.
  */
+@EnableWebSecurity
 public class SpringSecurityConfig {
+    @Configuration
+    @Order(1)
+    public class UserSpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http.antMatcher("/user/**")
+                    .authorizeRequests()
+                    .anyRequest().hasRole("USER")
+                    .and()
+                    .formLogin()
+                    .loginPage("/user-login")
+                    .failureUrl("/user-login?error")
+                    .permitAll();
+            // @formatter:on
+        }
+    }
+
     @Bean
     public static PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
