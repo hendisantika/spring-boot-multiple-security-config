abe7aa2a3bb7c7094b3699a2bef6bc7192b35f85
Hendi Santika <hendisantika@yahoo.co.id>
2021-11-03 06:39:57 +0700

    #4 - Add ApplicationController

---
+++ src/main/java/com/hendisantika/ApplicationController.java
@@ -0,0 +1,17 @@
+package com.hendisantika;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multiple-security-config
+ * User: TOSHIBA
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/3/2021
+ * Time: 6:39 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class ApplicationController {
+}
